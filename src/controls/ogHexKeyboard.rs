d246fc295d94e2d8cbd430c84094127522fec4a7
Maxim <maximveligan@gmail.com>
2017-09-04 13:05:34 -0700

    Started writing framework for controllers

---
+++ src/controls/ogHexKeyboard.rs
@@ -0,0 +1,38 @@
+pub enum sourceKeyPress {
+    1,
+    2,
+    3,
+    4,
+    5,
+    6,
+    7,
+    8,
+    9,
+    0,
+    A,
+    B,
+    C,
+    D,
+    E,
+    F
+}
+
+fn translate(keyPress: sourceKeyPress) {
+    match keyPress {
+        sourceKeyPress::1 => println!("Controller not yet implemented"), 
+        sourceKeyPress::2 => println!("Controller not yet implemented"), 
+        sourceKeyPress::3 => println!("Controller not yet implemented"), 
+        sourceKeyPress::4 => println!("Controller not yet implemented"), 
+        sourceKeyPress::5 => println!("Controller not yet implemented"), 
+        sourceKeyPress::6 => println!("Controller not yet implemented"), 
+        sourceKeyPress::7 => println!("Controller not yet implemented"), 
+        sourceKeyPress::8 => println!("Controller not yet implemented"), 
+        sourceKeyPress::9 => println!("Controller not yet implemented"), 
+        sourceKeyPress::0 => println!("Controller not yet implemented"), 
+        sourceKeyPress::A => println!("Controller not yet implemented"), 
+        sourceKeyPress::B => println!("Controller not yet implemented"), 
+        sourceKeyPress::C => println!("Controller not yet implemented"), 
+        sourceKeyPress::D => println!("Controller not yet implemented"), 
+        sourceKeyPress::E => println!("Controller not yet implemented"), 
+        sourceKeyPress::F => println!("Controller not yet implemented"), 
+    } 
